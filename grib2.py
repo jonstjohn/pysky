f87084800169e485dc982e04882614c4cbdb4fdb
Jon St. John <jonstjohn@gmail.com>
2012-12-07 22:16:52 -0700

    Added progress indicator for file downloads

--- grib2.py
+++ grib2.py
@@ -20,6 +20,8 @@ def download(data_dir):
     from dateutil import tz
     from dateutil.parser import parse
 
+    files_downloaded = False # whether files have been downloaded
+
     # Loop over directories that have forecast data files
     for dir in ['VP.001-003','VP.004-007']: # loop over remote directories
 
@@ -55,34 +57,38 @@ def download(data_dir):
 
                     remote_path = "%s/%s/%s" % (base_url, dir, filename)
                     local_path = "{0}/{1}/{2}".format(data_dir, dir, filename)
-                    local_time = os.stat(local_path).st_mtime
+                    local_time = os.stat(local_path).st_mtime if os.path.exists(local_path) else 0
 
-                    furl = urllib2.urlopen(remote_path)
-                    last_modified_str = furl.info()['Last-Modified']
+                    request = urllib2.urlopen(remote_path)
+                    last_modified_str = request.info()['Last-Modified']
                     remote_time = _utc2local(parse(last_modified_str))
 
                     # If file does not exist or the local file is older than the remote file, download
+                    info('Local file modified: {0} - Remote file modified: {1}'.format(local_time, remote_time))
                     if not os.path.exists(local_path) or local_time < remote_time:
-                        info('Downloading remote file')
-                        _download_file(furl, local_path)
+                        info('Downloading remote file {0}'.format(remote_path))
+                        _download_file(request, local_path)
                         os.utime(local_path, (remote_time, remote_time))
+                        files_downloaded = True
                     # Otherwise, just log some information
                     else:
-                        info('Local file modified: {0} - Remote file modified: {1}'.format(local_time, remote_time))
                         info('Local file is up-to-date, skipping download')
                     
-    # Cube data files if any were downloaded - TODO check to see if files were actually downloaded
-    cmd = "{degrib} {data_dir}/VP.001-003/*.bin {data_dir}/VP.004-007/*.bin -Data -Index {data_dir}/all.ind -out {data_dir}/all.dat".format(
-        degrib = degrib_path,
-        data_dir = data_dir
-    )
-    info(cmd)
-    output = ""
-    for line in os.popen(cmd).readlines():
+    # Cube data files if any were downloaded
+    if files_downloaded:
+        cmd = "{degrib} {data_dir}/VP.001-003/*.bin {data_dir}/VP.004-007/*.bin -Data -Index {data_dir}/all.ind -out {data_dir}/all.dat".format(
+            degrib = degrib_path,
+            data_dir = data_dir
+        )
+        info(cmd)
+        output = ""
+        for line in os.popen(cmd).readlines():
     
-        output += line
+            output += line
     
-    info(output)
+        info(output)
+    else:
+        info('No files downloaded - skipping cube')
 
 def xml(data_dir, latitude, longitude):
     """
@@ -120,10 +126,27 @@ def _utc2local(utc):
     utc = utc.replace(tzinfo = from_zone)
     return time.mktime(utc.astimezone(to_zone).timetuple())
 
-def _download_file(furl, local_path):
+def _download_file(request, local_path):
     """ Download file given a urllib2 urlopen from remote path to local path """
+    import sys
+    content_length = int(request.info()['Content-Length'])
+    chunk_size = 8192
+
     with open(local_path, 'wb') as f:
-        f.write(furl.read())
+        next_percentage = 5.0
+        chunk = request.read(chunk_size)
+        downloaded = 0
+        while chunk:
+            f.write(chunk)
+            downloaded += chunk_size
+            if (float(downloaded) / float(content_length)) * 100.0 > next_percentage:
+                sys.stdout.write("\r")
+                sys.stdout.write("{1}% complete {0}".format('#' * (int(next_percentage) / 5), int(next_percentage)))
+                next_percentage += 5.0
+                #sys.stdout.write('#')
+                sys.stdout.flush()
+            chunk = request.read(chunk_size)
+        print('')
 
 def info(str):
 
